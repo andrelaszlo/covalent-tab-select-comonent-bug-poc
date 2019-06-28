c3c8c3295e9995c9618f7874d1ba0d9552926778
andrelaszlo <andre@laszlo.nu>
2019-06-29 01:31:02 +0200

    Simplified example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,13 @@ import { TdTabSelectComponent } from '@covalent/core/tab-select';
 })
 export class AppComponent  {
   private tabs = ["First", "Second"];
-  private markForCheck = false;
   private selectedTab = this.tabs[0];
 
   addTab(tabSelect: TdTabSelectComponent) {
     this.tabs.push(`Tab ${this.tabs.length + 1}`);
-    if (this.markForCheck) {
-      tabSelect._changeDetectorRef.markForCheck();
-    }
   }
 
   removeTab(tabSelect: TdTabSelectComponent) {
     this.tabs.pop();
-    if (this.markForCheck) {
-      tabSelect._changeDetectorRef.markForCheck();
-    }
-  }
-
-  checkManually(tabSelect: TdTabSelectComponent) {
-    tabSelect._changeDetectorRef.markForCheck();
   }
 }
